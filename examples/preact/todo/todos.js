16c3c4722bc97174028a2627eec71e9989f21937
David Stone <davidjamesstone@gmail.com>
2018-12-19 14:10:27 +0000

    Rebuild

--- examples/preact/todo/todos.js
+++ examples/preact/todo/todos.js
@@ -68,20 +68,20 @@ export default class Todos extends Component {
 
 function view (props, state) {
   return h('section', null, [
-    h('form', { onsubmit: this.onSubmit }, [
-      h('input', { type: 'text', name: 'text', class: 'form-control', placeholder: 'Enter new todo', value: (state.input), required: 'required', autocomplete: 'off', onchange: this.onChangeInput }),
-      h('input', { type: 'checkbox', onchange: this.onChangeDoneAll })
+    h('form', { 'onsubmit': this.onSubmit }, [
+      h('input', { 'type': 'text', 'name': 'text', 'class': 'form-control', 'placeholder': 'Enter new todo', 'value': (state.input), 'required': 'required', 'autocomplete': 'off', 'onchange': this.onChangeInput }),
+      h('input', { 'type': 'checkbox', 'onchange': this.onChangeDoneAll })
     ]),
     h('ul', null, (state.todos || []).map(function ($value, $index, $target) {
       var todo = $value
-      return h('li', { key: (todo.id) }, [
+      return h('li', { 'key': (todo.id) }, [
         ($index + 1) + '.',
-        h('input', { type: 'text', value: (todo.text), onchange: e => this.onChangeText(e, todo), class: 'form-control', style: { borderColor: todo.text ? '' : 'red', textDecoration: todo.done ? 'line-through' : '' } }),
-        h('input', { type: 'checkbox', checked: (todo.done), onchange: e => this.onChangeDone(e, todo) })
+        h('input', { 'type': 'text', 'value': (todo.text), 'onchange': e => this.onChangeText(e, todo), 'class': 'form-control', 'style': { borderColor: todo.text ? '' : 'red', textDecoration: todo.done ? 'line-through' : '' } }),
+        h('input', { 'type': 'checkbox', 'checked': (todo.done), 'onchange': e => this.onChangeDone(e, todo) })
       ])
     }, this)),
     h('span', null, 'Total ' + (state.todos.length)),
-    state.todos.find(t => t.done) ? h('button', { onclick: this.onClickClear }, 'Clear done') : undefined,
+    state.todos.find(t => t.done) ? h('button', { 'onclick': this.onClickClear }, 'Clear done') : undefined,
     h('pre', null, h('code', null, (JSON.stringify(state, null, 2))))
   ])
 }
